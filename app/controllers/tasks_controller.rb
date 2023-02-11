a008a633297a0d510fd504412ebe68252134fa68
Dave Ng <wcng77@hotmail.com>
2023-02-11 18:02:28 +0800

    completed edit and update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
   end
@@ -22,10 +21,20 @@ class TasksController < ApplicationController
     @task.save
     redirect_to tasks_path
   end
-end
 
-private
+  def edit
+    @task = Task.find(params[:id])
+  end
 
-def task_params
-  params.require(:task).permit(:title, :details)
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
